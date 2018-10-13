436fd83882d06572a92ce4738a85ed9d5e6148f9
Ha3 <git@ha3.eu>
2018-10-13 11:41:46 +0200

    (evolve) get random num

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Input your guess:");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("Secret number is {}", secret_number);
 
     let mut guess = String::new();
+
+    println!("Input your guess:");
 
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed {}", guess)
+    println!("You guessed {}", guess);
 }
