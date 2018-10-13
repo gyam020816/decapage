b92a8f2fb182e12f7def3d602a7761bda3084746
Ha3 <git@ha3.eu>
2018-10-13 12:21:55 +0200

    (other) floating

--- ch03-variables/src/main.rs
+++ ch03-variables/src/main.rs
@@ -1,9 +1,7 @@
-const SOME_VALUE: u32 = 5;
-
 fn main() {
-    let x: u8 = 255;
-    let x: u8 = x+1;
+    let x: f32 = 1.2;
+    let y: f32 = 1.1;
+    let r = x - y;
 
-    println!("The value of x is: {}", x);
-    println!("Some value is: {}", SOME_VALUE);
+    println!("The value of r is: {}", r);
 }
