8f007cbffd76e666777ac47bb0e241bdb2d6d304
Ha3 <git@ha3.eu>
2018-10-13 11:59:23 +0200

    (evolve) match on result for parse

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,28 @@ fn main() {
 
     println!("Secret number is {}", secret_number);
 
-    let mut guess = String::new();
+    loop {
+        println!("Input your guess:");
 
-    println!("Input your guess:");
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed {}", guess);
+        println!("You guessed {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You're winner ! 🏆"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You're winner ! 🏆");
+                break;
+            }
+        }
     }
 }
