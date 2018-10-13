613e183b8e3510fa4d054c92cce219ef2472cc75
Ha3 <git@ha3.eu>
2018-10-13 12:00:09 +0200

    (evolve) remove secret display

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ use rand::Rng;
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Secret number is {}", secret_number);
-
     loop {
         println!("Input your guess:");
 
