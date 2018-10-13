db8b140fb68931ae5143d78325a986b1c9ba94b9
Ha3 <git@ha3.eu>
2018-10-13 11:02:43 +0200

    (evolve) ch2 guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Input your guess:");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed {}", guess)
 }
