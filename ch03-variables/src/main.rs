82bb83cead66e4fa7c67f10fe4b57490ca00a5f4
Ha3 <git@ha3.eu>
2018-10-13 12:18:07 +0200

    (evolve) panic

--- ch03-variables/src/main.rs
+++ ch03-variables/src/main.rs
@@ -1,9 +1,8 @@
 const SOME_VALUE: u32 = 5;
 
 fn main() {
-    let x = 5;
-    let x = x + 1;
-    let x = x * 2;
+    let x: u8 = 255;
+    let x: u8 = x+1;
 
     println!("The value of x is: {}", x);
     println!("Some value is: {}", SOME_VALUE);
