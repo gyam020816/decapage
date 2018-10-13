3aae94ed34b0cfc9c7616497e9f944efd780d97b
Ha3 <git@ha3.eu>
2018-10-13 12:13:02 +0200

    (evolve) const and shadowing

--- ch03-variables/src/main.rs
+++ ch03-variables/src/main.rs
@@ -1,6 +1,10 @@
+const SOME_VALUE: u32 = 5;
+
 fn main() {
-    let mut x = 5;
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+
     println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+    println!("Some value is: {}", SOME_VALUE);
 }
