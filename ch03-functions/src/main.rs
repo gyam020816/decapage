b2c98ce020098b09857a3bcb2fa4fa437d89e817
Ha3 <git@ha3.eu>
2018-10-14 11:19:09 +0200

    (evolve) functions

--- ch03-functions/src/main.rs
+++ ch03-functions/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
+
+    let mytuple = (3, 6);
+    another_function(mytuple);
+}
+
+fn another_function((x, y): (i32, i32)) {
+    println!("The value of x is {}.", x);
+    println!("The value of y is {}.", y);
 }
